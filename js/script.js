5c165c390d154f5128748295521c3dcb6f3f0d89
marcin1989 <marcinlubanski23@gmail.com>
2021-09-04 17:16:38 +0200

    warsztat-z-wersja-jq

---
+++ js/script.js
@@ -0,0 +1,62 @@
+console.log(`Warsztat - (jQuery) Pobierz dane po kliknieciu 
+przycisku`);
+
+    $(document).ready(function () {
+
+    $(`#get-data`).click(function () {
+
+        //wariant1
+        //$.get(`https://akademia108.pl/api/ajax/get-post.php`)
+        //.done(function(data){
+
+        //  let pId = $(`<p></p>`).text(`Post ID: ${data.id}`);
+        //let pUserId = $(`<p></p>`).text(`User ID: ${data.userId}`);
+        //let pTittle = $(`<p></p>`).text(`Title: ${data.title}`);
+        //let pBody = $(`<p></p>`).text(`Body: ${data.body}`);
+        //let hr = $(`<hr />`);
+
+        //let jqBody = $(`body`);
+
+        //jqBody.append(pId);
+        //jqBody.append(pUserId);
+        //jqBody.append(pTittle);
+        //jqBody.append(pBody);
+        //jqBody.append(hr);
+
+        //console.log(pId);
+        //})
+        // .fail(function(error){
+        //console.error(error);
+        //});
+
+        //});
+
+        //});
+
+        //wariant2
+        $.get(`https://akademia108.pl/api/ajax/get-post.php`)
+            .done(function (data) {
+
+                let pId = $(`<p></p>`).text(`Post ID: ${data.id}`);
+                let pUserId = $(`<p></p>`).text(`User ID: ${data.userId}`);
+                let pTittle = $(`<p></p>`).text(`Title: ${data.title}`);
+                let pBody = $(`<p></p>`).text(`Body: ${data.body}`);
+                let hr = $(`<hr />`);
+
+                let jqBody = $(`body`);
+
+                jqBody.append(pId);
+                jqBody.append(pUserId);
+                jqBody.append(pTittle);
+                jqBody.append(pBody);
+                jqBody.append(hr);
+
+
+            })
+            .fail(function (error) {
+                console.error(error);
+            });
+
+    });
+
+});
